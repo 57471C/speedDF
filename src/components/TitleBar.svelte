cbb016f3704f2a8329388fddc5c44e17cd13d467
57471C <terryminett@gmail.com>
2026-07-13 08:29:23 +1000

    style: differentiate Save As titlebar icon from core Save icon

--- src/components/TitleBar.svelte
+++ src/components/TitleBar.svelte
@@ -546,12 +546,10 @@
 
         <button onclick={onSaveAs} title="Save As... (Ctrl+Shift+S)" class="toolbar-btn">
           <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"/>
-            <polyline points="17 21 17 13 7 13 7 21"/>
-            <polyline points="7 3 7 8 15 8"/>
-            <line x1="20" y1="12" x2="20" y2="18"/>
-            <line x1="20" y1="18" x2="22" y2="16"/>
-            <line x1="20" y1="18" x2="18" y2="16"/>
+            <path d="M13.33 13H8a1 1 0 00-1 1v7" />
+            <path d="M14.363 17.634a2 2 0 00-.506.854l-.837 2.87a.5.5 0 00.62.62l2.87-.837a2 2 0 00.854-.506l4.013-4.009a1 1 0 10-3.004-3.004z" />
+            <path d="M7 3v4a1 1 0 001 1h7" />
+            <path d="M9 21H5a2 2 0 01-2-2V5a2 2 0 012-2h10.2a2 2 0 011.4.6l3.8 3.8a2 2 0 01.6 1.4v.3" />
           </svg>
         </button>
 
