af2c6f11e08a8d52e7222913edf7fafa621ed0d9
57471C <terryminett@gmail.com>
2026-07-19 23:32:57 +1000

    uprev to v1.0.3

--- src/components/HelpModal.svelte
+++ src/components/HelpModal.svelte
@@ -21,7 +21,7 @@
           >
           <span
             class="text-[10px] px-1.5 py-0.5 bg-slate-800 rounded font-mono text-slate-400"
-            >v1.0.2</span
+            >v1.0.3</span
           >
         </div>
         <button
